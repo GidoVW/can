0e5aae8f202da2e1d3dba0f351892612cf4ef98c
Gido van Wijk <gwijkvan@jaguarlandrover.com>
2018-04-19 08:57:56 -0700

    Add vcan() script to create virtual can interface

--- can.sh
+++ can.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
 can() {
   ./cancl &
   ./cansv
+}
+
+vcan() {
+  vcan_if_default="vcan0"
+  read -p "Name for vcan if (default $vcan_if_default): " vcan_if
+  vcan_if="${vcan_if:-$vcan_if_default}"
+  modprobe vcan
+  ip link add dev $vcan_if type vcan
+  ip link set up $vcan_if
 }
